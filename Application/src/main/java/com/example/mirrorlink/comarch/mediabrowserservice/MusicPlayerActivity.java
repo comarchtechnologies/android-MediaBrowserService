ffef2f1147ef0edaf7434249e22a2c4a845b892a
Mateusz Belicki <mateusz.belicki@comarch.com>
2015-09-18 10:17:24 +0200

    Enabled immersive mode.
    Conflicts:
    
    	Application/src/main/java/com/example/mirrorlink/comarch/mediabrowserservice/MusicPlayerActivity.java

--- Application/src/main/java/com/example/mirrorlink/comarch/mediabrowserservice/MusicPlayerActivity.java
+++ Application/src/main/java/com/example/mirrorlink/comarch/mediabrowserservice/MusicPlayerActivity.java
@@ -20,6 +20,7 @@ import android.app.Activity;
 import android.media.browse.MediaBrowser;
 import android.os.Bundle;
 
+import android.view.View;
 /**
  * Main activity for the music player.
  */
@@ -27,6 +28,26 @@ public class MusicPlayerActivity extends Activity
         implements BrowseFragment.FragmentDataHelper {
     public static Presenter presenter;
     private final String mMediaId = "__BY_GENRE__/Rock|-1679589699";
+
+
+    /* Enable immersive mode (hide software buttons on the bottom of the screen).
+     * This is considered to be a good MirrorLink app practice since the head unit
+     * is obliged to provide controls for Android buttons.
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        final int flags
+                = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
