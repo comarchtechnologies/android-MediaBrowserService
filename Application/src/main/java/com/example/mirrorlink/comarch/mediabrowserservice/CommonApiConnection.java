1b70d37a45481f98c8dbd0aed37a201bda16aa1a
Mateusz Belicki <mateusz.belicki@comarch.com>
2015-09-18 14:38:15 +0200

    Added requiered MirrorLink listeners.

--- Application/src/main/java/com/example/mirrorlink/comarch/mediabrowserservice/CommonApiConnection.java
+++ Application/src/main/java/com/example/mirrorlink/comarch/mediabrowserservice/CommonApiConnection.java
@@ -17,14 +17,63 @@ package com.example.mirrorlink.comarch.mediabrowserservice;
 
 import android.content.ComponentName;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 
 import com.mirrorlink.android.commonapi.Defs;
 import com.mirrorlink.android.commonapi.ICommonAPIService;
+import com.mirrorlink.android.commonapi.IConnectionListener;
+import com.mirrorlink.android.commonapi.IConnectionManager;
+import com.mirrorlink.android.commonapi.IContextListener;
+import com.mirrorlink.android.commonapi.IContextManager;
 
 public class CommonApiConnection implements ServiceConnection {
     ICommonAPIService mService;
+
+    /* MirrorLink connection related callbacks: */
+    IConnectionListener mConnectionListener = new IConnectionListener.Stub() {
+        @Override
+        public void onMirrorLinkSessionChanged(boolean sessionEstablished) throws RemoteException {
+            /* this will be called if head unit is connected or disconnected,
+             * sessionEstablished argument will be set to true when the connection is started
+             */
+        }
+
+        @Override
+        public void onAudioConnectionsChanged(Bundle audioConnections) throws RemoteException { }
+
+        @Override
+        public void onRemoteDisplayConnectionChanged(int remoteDisplayConnection) throws RemoteException { }
+    };
+    /* MirrorLink context information related callbacks: */
+    IContextListener mContextListener = new IContextListener.Stub() {
+        @Override
+        public void onAudioBlocked(int reason) throws RemoteException {
+            /* this will be called when the head unit blocks audio playback,
+             * the application should pause playback when this is called
+             */
+        }
+
+        @Override
+        public void onAudioUnblocked() throws RemoteException {
+            /* this will be called when the head unit stops blocking audio playback,
+             * the application should resume playback if the playback was previously
+             * paused by blocking event
+             */
+        }
+
+        @Override
+        public void onFramebufferBlocked(int reason, Bundle framebufferArea) throws RemoteException { }
+
+        @Override
+        public void onFramebufferUnblocked() throws RemoteException { }
+
+    };
+
+    IConnectionManager mConnectionManager;
+    IContextManager mContextManager;
+
     final String mPackageName;
 
     public CommonApiConnection(String packageName) {
@@ -39,6 +88,9 @@ public class CommonApiConnection implements ServiceConnection {
         ICommonAPIService mService = ICommonAPIService.Stub.asInterface(service);
         try {
             mService.applicationStarted(mPackageName, 1);
+            /* Registering connection and context listeners is mandatory: */
+            mConnectionManager = mService.getConnectionManager(mPackageName, mConnectionListener);
+            mContextManager = mService.getContextManager(mPackageName, mContextListener);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
